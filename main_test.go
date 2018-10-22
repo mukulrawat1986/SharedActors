0592aab2933471d981d915a7196583ca6f0b8b08
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-23 00:00:54 +0530

    Write test for function AskForNames
    AskForNames will take as input at least two names

--- main_test.go
+++ main_test.go
@@ -25,3 +25,18 @@ func TestAskForName(t *testing.T) {
 	a.Equal(len(main.ActorNames), 1)
 	a.Equal(main.ActorNames[0], "Mark")
 }
+
+func TestAskForNames(t *testing.T) {
+	setup()
+
+	a := assert.New(t)
+	b := []byte("Mark\nBrad\n")
+
+	r := bytes.NewBuffer(b)
+
+	main.AskForNames(r)
+
+	a.Equal(len(main.ActorNames), 2)
+	a.Equal(main.ActorNames[0], "Mark")
+	a.Equal(main.ActorNames[1], "Brad")
+}
