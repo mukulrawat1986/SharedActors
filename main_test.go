d1c9338e7e8340078e56528c585f8f4a3f5cee79
mukulrawat1986 <mukulsrawat@gmail.com>
2018-10-23 00:11:16 +0530

    convert our test to subtests and add a new subtest to ask for four names

--- main_test.go
+++ main_test.go
@@ -27,16 +27,36 @@ func TestAskForName(t *testing.T) {
 }
 
 func TestAskForNames(t *testing.T) {
-	setup()
 
-	a := assert.New(t)
-	b := []byte("Mark\nBrad\n")
+	t.Run("two names", func(t *testing.T) {
+		setup()
 
-	r := bytes.NewBuffer(b)
+		a := assert.New(t)
+		b := []byte("Mark\nBrad\n")
 
-	main.AskForNames(r)
+		r := bytes.NewBuffer(b)
 
-	a.Equal(len(main.ActorNames), 2)
-	a.Equal(main.ActorNames[0], "Mark")
-	a.Equal(main.ActorNames[1], "Brad")
+		main.AskForNames(r)
+
+		a.Equal(len(main.ActorNames), 2)
+		a.Equal(main.ActorNames[0], "Mark")
+		a.Equal(main.ActorNames[1], "Brad")
+	})
+
+	t.Run("four names", func(t *testing.T) {
+		setup()
+
+		a := assert.New(t)
+		b := []byte("Mark\nBrad\nJennifer\nMary\n")
+
+		r := bytes.NewBuffer(b)
+
+		main.AskForNames(r)
+
+		a.Equal(len(main.ActorNames), 4)
+		a.Equal(main.ActorNames[0], "Mark")
+		a.Equal(main.ActorNames[1], "Brad")
+		a.Equal(main.ActorNames[2], "Jennifer")
+		a.Equal(main.ActorNames[3], "Mary")
+	})
 }
